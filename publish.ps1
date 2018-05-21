4b6e1fadfe3bfc1d1f11a05b4c9022afd40f51de
Zeeko <vaezt@outlook.com>
2018-05-21 23:53:59 +0800

    debug ci config

--- publish.ps1
+++ publish.ps1
@@ -16,6 +16,8 @@ param(
 
 $tagReg = '(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-[a-zA-Z0-9-\.]+-(?<hash>.{7})'
 
+Write-Output "Git tag is $Tag"
+
 if ($Tag -match $tagReg) {
     $major = $Matches['major']
     $minor = $Matches['minor']
@@ -24,10 +26,15 @@ if ($Tag -match $tagReg) {
     Write-Output $ENV:DOCKER_PASSWORD | docker login -u $ENV:DOCKER_USERNAME --password-stdin
     docker tag "${ImageName}:tmp" "${ImageName}:$major.$minor.$patch"
     docker tag "${ImageName}:tmp" "${ImageName}:$major.$minor"
+    Write-Output "Pushing: ${ImageName}:$manjor.$minor.$patch"
     docker push "${ImageName}:$manjor.$minor.$patch"
+    Write-Output "Pushing: ${ImageName}:$manjor.$minor"
     docker push "${ImageName}:$manjor.$minor"
-    docker tag "${ImageName}:tmp" "${ImageName}:latest"
-    docker push "${ImageName}:latest"
+    if ($IsLatest) {
+        docker tag "${ImageName}:tmp" "${ImageName}:latest"
+        Write-Output "Pushing: ${ImageName}:$manjor.$minor"
+        docker push "${ImageName}:latest"
+    }
 
 }
 else {
