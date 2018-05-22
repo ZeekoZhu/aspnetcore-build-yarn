a1413491bb9066f243a2aff1462d8ffff7c1f714
Zeeko <vaezt@outlook.com>
2018-05-22 13:14:21 +0800

    SSR test

---
+++ samples/2.1/ClientApp/src/main.server.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { enableProdMode } from '@angular/core';
+import { renderModule, renderModuleFactory } from '@angular/platform-server';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { createServerRenderer } from 'aspnet-prerendering';
+
+export { AppServerModule } from './app/app.server.module';
+
+enableProdMode();
+
+export default createServerRenderer(params => {
+  const { AppServerModule, AppServerModuleNgFactory, LAZY_MODULE_MAP } = (module as any).exports;
+
+  const options = {
+    document: params.data.originalHtml,
+    url: params.url,
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP),
+      { provide: APP_BASE_HREF, useValue: params.baseUrl },
+      { provide: 'BASE_URL', useValue: params.origin + params.baseUrl }
+    ]
+  };
+
+  const renderPromise = AppServerModuleNgFactory
+    ? /* AoT */ renderModuleFactory(AppServerModuleNgFactory, options)
+    : /* dev */ renderModule(AppServerModule, options);
+
+  return renderPromise.then(html => ({ html }));
+});
