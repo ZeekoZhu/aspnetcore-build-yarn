5e2eee20f927cebba5ff661e21bb68ca354d318f
Zeeko <vaezt@outlook.com>
2018-05-22 14:10:09 +0800

    fix publish script error

--- publish.ps1
+++ publish.ps1
@@ -18,28 +18,6 @@ param(
     $TagSuffix
 )
 
-Write-Output "Push images for ${ImageName}:$Tag-$TagSuffix [$IsLatest]$"
-
-if ($Tag -match $tagReg) {
-    $version = Get-Version $Tag
-    $major = $version.Major
-    $minor = $version.Minor
-    $patch = $version.Patch
-    $preRelease = $version.PreRelease
-    Write-Output "Version is $major.$minor.$patch"
-    Write-Output $ENV:DOCKER_PASSWORD | docker login -u $ENV:DOCKER_USERNAME --password-stdin
-    Push-Image -ImageName $ImageName -Version "$major.$minor.$patch$preRelease" -Suffix $TagSuffix
-    Push-Image -ImageName $ImageName -Version "$major.$minor" -Suffix $TagSuffix
-    if ($IsLatest) {
-        Push-Image -ImageName $ImageName -Version 'latest'
-    }
-
-}
-else {
-    Write-Error "Invalid git tag"
-    exit 1
-}
-
 function Get-Version {
     param(
         # Tag
@@ -91,4 +69,27 @@ function Push-Image {
     Write-Output "Pushing: $tag"
     docker push $tag
 
+}
+
+
+Write-Output "Push images for ${ImageName}:$Tag-$TagSuffix [$IsLatest]$"
+
+if ($Tag -match $tagReg) {
+    $version = Get-Version $Tag
+    $major = $version.Major
+    $minor = $version.Minor
+    $patch = $version.Patch
+    $preRelease = $version.PreRelease
+    Write-Output "Version is $major.$minor.$patch"
+    Write-Output $ENV:DOCKER_PASSWORD | docker login -u $ENV:DOCKER_USERNAME --password-stdin
+    Push-Image -ImageName $ImageName -Version "$major.$minor.$patch$preRelease" -Suffix $TagSuffix
+    Push-Image -ImageName $ImageName -Version "$major.$minor" -Suffix $TagSuffix
+    if ($IsLatest) {
+        Push-Image -ImageName $ImageName -Version 'latest'
+    }
+
+}
+else {
+    Write-Error "Invalid git tag"
+    exit 1
 }
