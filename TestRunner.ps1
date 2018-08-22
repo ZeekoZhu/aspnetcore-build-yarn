05eac5875e7a6497b159a2e1b16ba057652e234b
Zeeko <vaezt@outlook.com>
2018-08-22 09:41:19 +0800

    💚 Fix ci tests

--- TestRunner.ps1
+++ TestRunner.ps1
@@ -10,6 +10,10 @@ $specConfigFile = Get-Content ./spec.json -Raw
 
 $specConfig = ConvertFrom-Json $specConfigFile
 
-$spec = $spec."$Job"
+$spec = $specConfig."$Job"
 
-Test.ps1 $spec.testImage -Dotnet $spec.dotnet -Node $spec.node -Yarn $spec.yarn -Sdk:$spec.sdk
+& ./Test.ps1 $spec.testImage -Dotnet $spec.dotnet -Node $spec.node -Yarn $spec.yarn -Sdk:$spec.sdk
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Test Faild"
+}
