050435fff508512078af17139d0b825121949dbf
Jerry <jerry@mail.cnblogs.com>
2018-11-26 14:59:10 +0800

    🐛 Exit 1 when error happens

--- Test.ps1
+++ Test.ps1
@@ -38,19 +38,23 @@ $yarnResult = docker run --rm $Image yarn --version
 Write-Output "dotnet version should be $Dotnet"
 if ($dotnetResult -ne $Dotnet) {
     Write-Error "Checking dotnet failed:`n$dotnetResult"
+    exit 1
 }
 
 Write-Output "node.js version should be $Node"
 if ($nodeResult -ne "v$Node") {
     Write-Error "Checking node.js failed:`n$nodeResult"
+    exit 1
 }
 
 Write-Output "yarn version should be $Yarn"
 if ($yarnResult -ne $Yarn) {
     Write-Error "Checking yarn failed:`n$yarnResult"
+    exit 1
 }
 
 
 if ($LASTEXITCODE -ne 0) {
     Write-Error 'Test failed'
+    exit 1
 }
