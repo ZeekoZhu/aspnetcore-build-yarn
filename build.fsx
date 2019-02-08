16b605abb4d2a7216a7e31122fd8d5b50e1831d6
ZeekoZhu <vaezt@outlook.com>
2019-02-08 17:37:12 +0800

    ♻️ Replace pwsh with fake

--- build.fsx
+++ build.fsx
@@ -1,9 +1,9 @@
-open System.Diagnostics
 #load ".fake/build.fsx/intellisense.fsx"
 #if !FAKE
   #r "Facades/netstandard"
 #endif
 open System
+open System.Text.RegularExpressions
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
@@ -13,10 +13,38 @@ open Fake.Core.TargetOperators
 open Fake.Tools
 open CommandLine
 open CommandLine.Text
+open Nett
+
+// ----------------------
+// Var
+// ----------------------
+
+let BuildParams = "BuildParams"
+
 
 // ----------------------
 // Utils
 // ----------------------
+
+let showOutput =
+    CreateProcess.redirectOutput
+    >> (CreateProcess.withOutputEventsNotNull Trace.log Trace.traceError)
+    >> CreateProcess.ensureExitCode
+
+let runCmd file args =
+    CreateProcess.fromRawCommand file args
+    |> showOutput
+    |> Proc.run
+    |> ignore
+
+let runCmdAndReturn file args =
+    let result =
+        CreateProcess.fromRawCommand file args
+        |> showOutput
+        |> Proc.run
+    result.Result.Output
+
+let dockerCmd (subCmd: string) (args: string list) = runCmd "docker" (subCmd::args)
 
 let runGitCmd command =
     let (success, stdout, stderr) = Git.CommandHelper.runGitCommand "./" command
@@ -30,12 +58,20 @@ let getLatestTag () =
         runGitCmd (sprintf "describe --tags %s" revListResult)
     tagName
 
+let handleCli<'t> (args: seq<string>) (fn: 't -> unit) =
+    let parseResult =
+        Parser.Default.ParseArguments<'t> args
+    match parseResult with
+    | :? Parsed<'t> as parsed -> fn parsed.Value
+    | :? NotParsed<'t> as notParsed ->
+        failwithf "Invalid: %A, Errors: %A" args notParsed.Errors
+    | _ -> failwith "Invalid parser result"
 
 // ----------------------
 // Command Line Interface
 // ----------------------
 
-module Cli =
+module TriggerCI =
     type TriggerCIOptions =
         { [<Option('v', "version", Required = true)>] Version: string
           [<Option('l', "latest", Default = false)>] IsProd: bool
@@ -74,23 +110,140 @@ module Cli =
         ()
 
     let triggerCiCli (args: seq<string>) =
-        let parseResult =
-            Parser.Default.ParseArguments<TriggerCIOptions> args
-        match parseResult with
-        | :? Parsed<TriggerCIOptions> as parsed -> triggerCi parsed.Value
-        | :? NotParsed<TriggerCIOptions> as notParsed ->
-            Trace.traceErrorfn "Invalid: %A, Errors: %A" args notParsed.Errors
-        | _ -> failwith "Invalid parser result"
+        handleCli args triggerCi
+
+module Docker =
+    open System.IO
+
+    type BuildOptions =
+        { [<Option('t', Required = true)>] Tag: string
+          [<Option('f', Required = true)>] Dockerfile: string
+          [<Option('c', Required = true)>] ContextPath: string
+        }
+
+    let build (options: BuildOptions) =
+        dockerCmd "build"
+            [ "-t"; options.Tag; "-f"; options.Dockerfile; options.ContextPath ]
+
+    [<CLIMutable>]
+    type ImageSpecItem =
+        { Name: string
+          TestImage: string
+          Dotnet: string
+          Node: string
+          Yarn: string
+          Sdk: bool
+          Suffix: string
+          Latest: bool
+          SkipTest: bool
+          Tag: string
+        }
+
+    [<CLIMutable>]
+    type ImageSpec =
+        { Images: ImageSpecItem [] }
+
+    let getBuildParams (name: string) =
+        let spec = Toml.ReadFile<ImageSpec>("./spec.toml")
+        spec.Images |> Seq.find (fun x -> x.Name = name)
+
+    let testImage () =
+        let imageSpec = FakeVar.getOrFail<ImageSpecItem> BuildParams
+        if imageSpec.SkipTest then ()
+        else
+            let dockerRunRm args =
+                runCmdAndReturn "docker" (["run"; "--rm"; imageSpec.TestImage; ] @ args)
+
+            let checkVersion item itemValue actualValue =
+                Trace.tracefn "Q: Is %s's version %s?" item itemValue
+                if itemValue = actualValue
+                then Trace.traceErrorfn "A: No, %s's version is %s!" item actualValue
+                else Trace.tracefn "A: Yes!"
+
+            let dotnetVersion =
+                if imageSpec.Sdk
+                then dockerRunRm ["dotnet"; "--version"]
+                else
+                    let reg = Regex("^  Version: (?<runtime>.*)$", RegexOptions.Multiline)
+                    let result = dockerRunRm ["dotnet"; "--info"]
+                    (reg.Match(result).Groups.Item "runtime").Value
+
+            let nodeVersion = 
+                dockerRunRm ["node"; "--version"]
+
+            let yarnVersion =
+                dockerRunRm ["yarn"; "--version"]
+
+            [ ("dotnet", dotnetVersion, imageSpec.Dotnet)
+              ("node.js", nodeVersion, imageSpec.Node)
+              ("yarn", yarnVersion, imageSpec.Yarn)
+            ]
+            |> List.iter (fun (item, actual, expected) -> checkVersion item expected actual)
+
+
+    let login () =
+        let dockerPwd = Environment.environVar "DOCKER_PASSWORD"
+        let dockerUsr = Environment.environVar "DOCKER_USERNAME"
+        let input = StreamRef.Empty
+        use inputWriter = new StreamWriter(input.Value)
+        inputWriter.Write dockerPwd
+        
+        CreateProcess.fromRawCommand
+            "docker" [ "login"; "-u"; dockerUsr; "--password-stdin" ]
+        |> CreateProcess.withStandardInput (CreatePipe input)
+        |> showOutput
+        |> Proc.run
+        |> ignore
+
+    let publishImage () =
+        let spec = FakeVar.getOrFail<ImageSpecItem> BuildParams
+        login ()
+        let version = SemVer.parse spec.Dotnet
+        let major = string version.Major
+        let minor = major + "." + string version.Minor
+        let patch = minor + "." + string version.Patch
+        seq {
+            yield major
+            yield minor
+            yield patch
+            if spec.Latest then yield "latest"
+        }
+        |> Seq.map (fun t ->
+             let tag = spec.Tag + ":" + t
+             if String.isNullOrEmpty spec.Suffix then tag
+             else tag + "-" + spec.Suffix
+           )
+        |> Seq.iter (fun t ->
+            Trace.tracefn "%s" t
+            //dockerCmd "tag" [ spec.TestImage; t ]
+            //dockerCmd "push" [ t ]
+           )
 
 // ----------------------
 // Targets
 // ----------------------
 
 let triggerCi (p: TargetParameter) =
-    Cli.triggerCiCli p.Context.Arguments
+    TriggerCI.triggerCiCli p.Context.Arguments
+ 
+let ciBuild (p: Docker.BuildOptions) =
+    let buildParams = Docker.getBuildParams p.Tag
+    let buildOptions = { p with Tag = buildParams.TestImage }
+    Docker.build buildOptions
+    FakeVar.set BuildParams buildParams
 
 Target.create "TriggerCI" triggerCi
 
-Target.create "All" ignore
+Target.create "CI:Build" (fun p -> handleCli p.Context.Arguments ciBuild)
 
-Target.runOrDefaultWithArguments "All"
+Target.create "CI:Test" (fun _ -> Docker.testImage())
+
+Target.create "CI:Publish" (fun _ -> Docker.publishImage())
+
+Target.create "CI" ignore
+
+"CI:Build" ==> "CI:Test" ==> "CI:Publish" ==> "CI"
+
+Target.create "Empty" ignore
+
+Target.runOrDefaultWithArguments "Empty"
